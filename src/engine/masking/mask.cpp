ccdb98129e6bea56ed361affd946e17e59b9e05d
texidot <alexandre.grosdidier@edu.esiee.fr>
2026-01-19 19:45:46 +0100

    added functions to make low res mask from image

--- src/engine/masking/mask.cpp
+++ src/engine/masking/mask.cpp
@@ -12,6 +12,23 @@ int** createMask(const cv::Mat& image) {
     return mask;
 }
 
+int** createMaskLowRes(const cv::Mat& image, int cell_size, int threshold) { // create a low res mask based on cell size
+    // resize to low resolution
+    int low_res_rows = image.rows / cell_size;
+    int low_res_cols = image.cols / cell_size;
+    cv::Mat low_res_image;
+    cv::resize(image, low_res_image, cv::Size(low_res_cols, low_res_rows), 0, 0, cv::INTER_AREA);
+    // create mask based on low res image
+    int** mask = new int*[low_res_rows];
+    for (int i = 0; i < low_res_rows; ++i) {
+        mask[i] = new int[low_res_cols];
+        for (int j = 0; j < low_res_cols; ++j) {
+            mask[i][j] = (low_res_image.at<uchar>(i, j) > threshold) ? 1 : 0;
+        }
+    }
+    return mask;
+}
+
 // create a greyscale image for debug purposes based on the mask
 cv::Mat applyMask(int** mask, int res_x, int res_y) {
     cv::Mat maskedImage(res_x, res_y, CV_8UC1, cv::Scalar(0));
